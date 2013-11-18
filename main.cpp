8f1332bfec5bf7f1f970a0b7075ac77c66b854a0
Youngbin Kim <goodzlmn@naver.com>
2013-11-18 12:50:37 +0900

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "pplwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    PPLWindow w;
+    w.show();
+    
+    return a.exec();
+}
